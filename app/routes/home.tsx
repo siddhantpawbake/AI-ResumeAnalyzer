0b305c935a7ebd0faca9edc87ea299271dc8cd86
siddhantpawbake <sidpawbake15@gmail.com>
2026-01-13 12:01:16 +0530

    Feat: Added Navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMind" },
+    { name: "description", content: "AI feedback for your dream Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your Applications & Resume score</h1>
+        <h2>Review your sumbissions and check AI powered feedback.</h2>
+      </div>
+    </section>
+    </main>;
 }
