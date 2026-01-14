86ea9af48b5efd833598b31d42c78b943521d443
siddhantpawbake <sidpawbake15@gmail.com>
2026-01-14 18:13:19 +0530

    feat: implement authentication flow and integrate Zustand for state management

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants/index";
 import ResumeCard from "~/components/resumecard";
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import { usePuterStore } from '~/lib/puter'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,9 +14,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {auth}=usePuterStore();
+    const navigate=useNavigate();
+    useEffect(()=>{
+        if(!auth.isAuthenticated) navigate('auth?next=/');
+    },[auth.isAuthenticated])
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+      
       <section className="main-section">
         <div className="page-heading py-12">
           <h1>Track your Applications & Resume score</h1>
