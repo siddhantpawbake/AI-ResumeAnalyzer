a7771185f9d5ac24cd004ddc9f48ee8b45ce5f35
siddhantpawbake <sidpawbake15@gmail.com>
2026-01-21 15:59:08 +0530

    feat: implement form handling in Upload component for resume submission

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -4,7 +4,16 @@ import Navbar from '~/components/Navbar'
 const Upload = () => {
     const[isProcessing,setIsProcessing]=useState();
     const[statusText,setStatusText]=useState();
-    const handleSubmit=(e:FormEvent<HTMLFormElement>)=>{}
+    const handleSubmit=(e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        const form=e.currentTarget.closest('form');
+        if(!form) return;
+        const formData=new FormData(form);
+        const companyName=formData.get('company-name');
+        const jobTitle=formData.get('job-title');
+        const jobDescription=formData.get('job-description');
+
+    }
     const[file,setFile]=useState<File|null> (null);
     const handleFileSelect=(file:File|null)=>{
         setFile(file)
